49f0ee8662d249b3c8f2901443a4502e8154cbf5
Ofek <ofekikotrat@gmail.com>
2021-12-21 00:47:26 +0200

    Created comparison candle

--- utils/random/random_condition_creators.go
+++ utils/random/random_condition_creators.go
@@ -6,6 +6,13 @@ import (
 	"math/rand"
 )
 
+var (
+	randConditionCreators = []func(int) condition.ICondition{
+		CreateRandomCandleTypeCondition,
+		CreateRandomCandleComparisonCondition,
+	}
+)
+
 func CreateRandomCandleTypeCondition(streamsize int) condition.ICondition {
 	n := rand.Intn(streamsize)
 	isGreen := rand.Intn(2)
@@ -14,4 +21,15 @@ func CreateRandomCandleTypeCondition(streamsize int) condition.ICondition {
 		return condition_list.CandleTypeCondition{CandleIndex: n, IsGreen: true}
 	}
 	return condition_list.CandleTypeCondition{CandleIndex: n, IsGreen: false}
+}
+
+func CreateRandomCandleComparisonCondition(streamsize int) condition.ICondition {
+	candleParts := []string{"open", "high", "close", "low"}
+	index1 := rand.Intn(streamsize)
+	index2 := rand.Intn(streamsize)
+	part1 := candleParts[rand.Intn(len(candleParts))]
+	part2 := candleParts[rand.Intn(len(candleParts))]
+	percentage := (rand.Float32() - 0.5) * 200
+	return condition_list.CandleComparisonCondition{CandleIndex1: index1, CandlePart1: part1,
+		CandleIndex2: index2, CandlePart2: part2, Percentage: percentage}
 }
