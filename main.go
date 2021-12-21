53ad1f6f536109fdf0c4cb1753b596f6fcb55002
Ofek <ofekikotrat@gmail.com>
2021-12-22 00:33:17 +0200

    Created  arguments parser and changed main likely

--- main.go
+++ main.go
@@ -1,43 +1,24 @@
 package main
 
 import (
+	"branch_learning/args_parser"
 	candle_stream "branch_learning/candle_stream"
 	"branch_learning/evolutioner"
 	log_init "branch_learning/log_initializer"
-	"branch_learning/output"
-	"branch_learning/utils/random"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
+	configuration := args_parser.InitArgs()
+	cs := candle_stream.LoadCandleStreamFromCsv(configuration.DataFile)
 
-	cs := candle_stream.LoadCandleStreamFromCsv("data/data.csv")
-	randomConfig := random.RandomStrategyConfig{
-		WindowMin:          10,
-		WindowMax:          30,
-		ExitMin:            0.5,
-		ExitMax:            1.5,
-		ConditionNumberMin: 1,
-		ConditionNumberMax: 3,
+	switch configuration.CallType {
+	case "test": // TODO: Create a mechanism for testing strategies (load strategy, and test it)
+		break
+	default:
+		log_init.LogInitialize(configuration.OutputConfig)
+		evolutioner.Evolve(cs, configuration.EvolutionConfig, configuration.OutputConfig)
 	}
-
-	configuration := evolutioner.EvolutionConfig{
-		GenerationSize:       100,
-		NumEvolutions:        10,
-		OldPercentage:        0.1,
-		MutatePercentage:     0.3, // 30%
-		ReproducedPercentage: 0.3, // 30%
-		RandomPercentage:     0.3,
-
-		RandomConfig:         randomConfig,
-		ExitMutateMultiplier: 5,
-		WindowSizeMultiplier: 5,
-	}
-
-	outputConfig := output.OutputConfig{
-		LogFile:           "",
-		PrintFrequency:    "foreach",
-		PrintBestStrategy: false,
-	}
-	log_init.LogInitialize(&outputConfig)
-	evolutioner.Evolve(cs, &configuration, &outputConfig)
 }
