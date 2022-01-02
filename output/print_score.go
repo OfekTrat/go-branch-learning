cafb03d7c5154a9919a49dfa12b3faf3930dd63d
Ofek <ofekikotrat@gmail.com>
2022-01-02 19:38:47 +0200

    Changed output mechanism

--- output/print_score.go
+++ output/print_score.go
@@ -14,5 +14,7 @@ func PrintScore(backtester *bt.BackTester) {
 
 	log.Printf("Score: %v\n", score)
 	log.Printf("Wins: %v, Losses: %v\n", wins, losses)
-	log.Printf("TakeProfit %v, StopLoss: %v\n", takeProfit, stopLoss)
+	log.Printf("TakeProfit|StopLoss: %v|%v\n", takeProfit, stopLoss)
+	log.Printf("Win Rate: %v\n", float32(wins)/float32((wins+losses))*100)
+	log.Printf("Reward/Risk: %v\n", takeProfit/stopLoss)
 }
--- output/print_strategy.go
+++ output/print_strategy.go
@@ -7,7 +7,14 @@ import (
 	"strings"
 )
 
-func PrintStrategyConditions(strategy *st.Strategy) {
+func PrintStrategy(strategy *st.Strategy) {
+	log.Printf("StopLoss: %v\n", strategy.StopLoss())
+	log.Printf("TakeProfit: %v\n", strategy.TakeProfit())
+	log.Printf("Window: %v\n", strategy.WindowSize())
+	printStrategyConditions(strategy)
+}
+
+func printStrategyConditions(strategy *st.Strategy) {
 	conditionStrings := []string{}
 	for _, cond := range strategy.Conditions().ToList() {
 		conditionStrings = append(conditionStrings, condition.ConditionToJson(cond))
