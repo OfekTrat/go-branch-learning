db4cea5e015b7481253b82d76fa73ccd3690739d
Ofek <ofekikotrat@gmail.com>
2022-01-14 09:58:23 +0200

    Changed output of result, strategy, conditions to json

--- parser/strategy_parser.go
+++ parser/strategy_parser.go
@@ -3,59 +3,32 @@ package parser
 import (
 	"branch_learning/condition"
 	st "branch_learning/strategy"
-	"fmt"
+	"encoding/json"
+	"log"
 	"os"
-	"regexp"
-	"strconv"
 )
 
-var (
-	parametersPattern = `(?s)StopLoss: (?P<StopLoss>[\d\.]*).*TakeProfit: (?P<TakeProfit>[\d\.]*).*Window: (?P<Window>\d*)`
-	conditionPattern  = `(?s)({.*?})+`
-)
-
-func ParseStrategy(strategyStr string) *st.Strategy {
-	params := getParams(parametersPattern, strategyStr)
-	validateParams(params)
-	stoploss, slErr := strconv.ParseFloat(params["StopLoss"], 32)
-	takeprofit, tpErr := strconv.ParseFloat(params["TakeProfit"], 32)
-	window, wErr := strconv.ParseInt(params["Window"], 10, 32)
-
-	if slErr != nil || tpErr != nil || wErr != nil {
-		fmt.Println("Wrong values")
+func ParseStrategy(strategyStr []byte) *st.Strategy {
+	mappedStrategy := make(map[string]interface{})
+	err := json.Unmarshal(strategyStr, &mappedStrategy)
+	if err != nil {
+		log.Fatal(err)
 		os.Exit(1)
 	}
-	conditions := parseConditions(strategyStr)
-	return st.CreateStrategy(int(window), float32(takeprofit), float32(stoploss), conditions)
+	stopLoss := float32(mappedStrategy["stop_loss"].(float64))
+	takeProfit := float32(mappedStrategy["take_profit"].(float64))
+	window := int(mappedStrategy["window"].(float64))
+	conditions := ParseConditions(mappedStrategy["conditions"].([]interface{}))
+	strategy := st.CreateStrategy(window, takeProfit, stopLoss, conditions)
+	return strategy
 }
 
-func getParams(pattern, data string) (paramsMap map[string]string) {
-	var compRegEx = regexp.MustCompile(pattern)
-	match := compRegEx.FindStringSubmatch(data)
-
-	paramsMap = make(map[string]string)
-	for i, name := range compRegEx.SubexpNames() {
-		if i > 0 && i <= len(match) {
-			paramsMap[name] = match[i]
-		}
+func ParseConditions(conditions []interface{}) *condition.Conditions {
+	conditionList := []condition.ICondition{}
+	for _, cond := range conditions {
+		mapCond := cond.(map[string]interface{})
+		assertedCond := assertCond(mapCond)
+		conditionList = append(conditionList, assertedCond)
 	}
-	return paramsMap
-}
-
-func validateParams(params map[string]string) {
-	if params["StopLoss"] == "" || params["TakeProfit"] == "" || params["Window"] == "" {
-		fmt.Println("Missing parameters - check file")
-		os.Exit(1)
-	}
-}
-
-func parseConditions(data string) *condition.Conditions {
-	compRegex := regexp.MustCompile(conditionPattern)
-	matches := compRegex.FindAllString(data, -1)
-	conditions := condition.Conditions{}
-	for _, match := range matches {
-		cond := ParseConditionFromJson(match)
-		conditions.Add(cond)
-	}
-	return &conditions
+	return condition.CreateConditions(conditionList)
 }
